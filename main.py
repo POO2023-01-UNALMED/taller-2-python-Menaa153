92e02572d027b3f94fae81e7ca3052f3fae86b51
Menaa153 <cimbacuan@unal.edu.co>
2023-02-20 22:42:19 -0500

    test

---
+++ main.py
@@ -0,0 +1,51 @@
+class Auto:
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+    
+    def cantidadAsientos(self):
+        numeroAsientos = 0
+        for asiento in self.asientos:
+            if isinstance(asiento, Asiento):
+                numeroAsientos += 1
+        return numeroAsientos
+    
+    def verificarIntegridad(self):
+        for asiento in self.asientos:
+            if asiento != None:
+                if asiento.registro != self.registro:
+                    return "Las piezas no son originales"
+        if self.motor.registro != self.registro:
+            return "Las piezas no son originales"
+        
+        return "Auto original"
+
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+        
+    def cambiarColor(self, color):
+        colores = ["rojo", "verde", "amarillo", "negro", "blanco"]
+        if color in colores:
+            self.color = color
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registros):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registros = registros
+    
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+    
+    def asignarTipo(self, tipo):
+        tipos =["electricos", "gasolina"]
+        if tipo in tipos:
+            self.tipo = tipo
